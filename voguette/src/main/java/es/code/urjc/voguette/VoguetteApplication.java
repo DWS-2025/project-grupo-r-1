08c9413aab0ca4bc529154b40ccb957f06cfa292
jwang882 <j.wang.2021@alumnos.urjc.es>
2025-02-11 10:09:07 +0100

    Creación proyecto Maven

---
+++ voguette/src/main/java/es/code/urjc/voguette/VoguetteApplication.java
@@ -0,0 +1,13 @@
+package es.code.urjc.voguette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoguetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoguetteApplication.class, args);
+	}
+
+}
---
+++ voguette/src/test/java/es/code/urjc/voguette/VoguetteApplicationTests.java
@@ -0,0 +1,13 @@
+package es.code.urjc.voguette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoguetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
